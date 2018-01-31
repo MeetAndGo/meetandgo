5e842613a5dbc372785eddd453f0f891ef2d24c0
Manu Gil <madtriangl3@gmail.com>
2018-01-31 15:23:26 +0000

    ~ Changed database functionality (user exists) + User class + Introduced object passing to database

--- app/src/main/java/com/meetandgo/meetandgo/MainActivity.java
+++ app/src/main/java/com/meetandgo/meetandgo/MainActivity.java
@@ -5,22 +5,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.firebase.client.Firebase;
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int RC_SIGN_IN = 123;
     private FirebaseAuth mAuth;
-
+    private String uid;
+    private boolean exists = false;
 
     List<AuthUI.IdpConfig> providers = Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),new AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER).build());
 
@@ -55,11 +62,36 @@ public class MainActivity extends AppCompatActivity {
             if (resultCode == RESULT_OK) {
                 // Successfully signed in
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                String uid = user.getUid();
+                uid = user.getUid();
+
                 // Write a message to the database
                 FirebaseDatabase database = FirebaseDatabase.getInstance();
-                DatabaseReference myRef = database.getReference("user_id");
-                myRef.setValue(uid);
+
+                //Check if user already exists
+                DatabaseReference myRef = database.getReference("users");
+                myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot snapshot) {
+                        if (snapshot.hasChild(uid)) {
+                            exists = true;
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
+                //Add user to database
+                if(!exists) {
+                    Map<String, User> users = new HashMap<>();
+                    users.put(uid, new User(user.getDisplayName(), user.getEmail(), 0.0, null));
+                    myRef.setValue(users);
+                }
+
+
+
                 // ...
                 //startActivity();
             } else {
