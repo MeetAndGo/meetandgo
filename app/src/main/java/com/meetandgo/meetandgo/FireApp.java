dcbc1cdafec89b70f69f84c39b3383b00cb392dc
Manu Gil <madtriangl3@gmail.com>
2018-01-31 14:35:07 +0000

    ~ Database Properly Connected + Basic Writing to database implemented (needs testing)

---
+++ app/src/main/java/com/meetandgo/meetandgo/FireApp.java
@@ -0,0 +1,21 @@
+package com.meetandgo.meetandgo;
+
+import android.app.Application;
+
+import com.firebase.client.Firebase;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.*;
+
+/**
+ * Created by gilmarma on 1/31/2018.
+ */
+
+public class FireApp extends Application {
+    public void onCreate(){
+        super.onCreate();
+       // Fireb
+        Firebase.setAndroidContext(this);
+    }
+}
