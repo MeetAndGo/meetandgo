544033d36aca37955d5ebf81dfaa99e3faa569d5
Manu Gil <madtriangl3@gmail.com>
2018-02-22 15:24:30 +0000

    + Location Class for Firebase

---
+++ app/src/main/java/com/meetandgo/meetandgo/data/Loc.java
@@ -0,0 +1,37 @@
+package com.meetandgo.meetandgo.data;
+
+/**
+ * Loc class to upload latitude and longitude to firebase
+ */
+public class Loc {
+
+    private double lat ;
+    private double lng ;
+
+    public Loc() {
+        this.lat = 0;
+        this.lng = 0;
+    }
+
+    public Loc(double lat, double lng){
+        this.lat = lat;
+        this.lng = lng;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLng() {
+        return lng;
+    }
+
+    public void setLng(double lng) {
+        this.lng = lng;
+    }
+
+}
