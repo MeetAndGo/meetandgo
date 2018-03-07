825f34f40d74015e49052cc32f9e98fe4aaaa451
gilmarma <manugildev@gmail.com>
2018-03-07 15:58:21 +0000

    ~ modify chat layout and interactions

--- app/src/main/java/com/meetandgo/meetandgo/fragments/ChatsFragment.java
+++ app/src/main/java/com/meetandgo/meetandgo/fragments/ChatsFragment.java
@@ -2,33 +2,24 @@ package com.meetandgo.meetandgo.fragments;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.FirebaseDatabase;
 import com.meetandgo.meetandgo.R;
-import com.meetandgo.meetandgo.data.ChatMessage;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-import static com.facebook.FacebookSdk.getApplicationContext;
-
 public class ChatsFragment extends Fragment {
-    @BindView(R.id.drawer_layout) DrawerLayout mDrawerLayout;
-    @BindView(R.id.navigation) NavigationView mNavView;
-    @BindView(R.id.toolbar) Toolbar mToolbar;
-    @BindView(R.id.fab) FloatingActionButton fab;
+
+    @BindView(R.id.msg_button) FloatingActionButton mSendMsgButton;
+    @BindView(R.id.input) EditText mTextInput;
 
     private static final String TAG = "ChatsFragment";
+    private View mView;
 
     public ChatsFragment() {}
 
@@ -41,7 +32,19 @@ public class ChatsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chats, container, false);
+        mView = inflater.inflate(R.layout.fragment_chats, container, false);
+        ButterKnife.bind(this, mView);
+
+        mSendMsgButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText input = mTextInput;
+
+                //FirebaseDB.addMessageToJourney(, input)
+            }
+        });
+
+        return mView;
     }
 
     public void onStart() {
